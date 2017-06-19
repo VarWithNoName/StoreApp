9cf573d00e125698b6a2d50f77f07b7eff0fb863
Narbeth <lazowskilejanhd@gmail.com>
2017-06-18 21:23:59 -0500

    Redid routes

--- src/DeliAppClient/src/app/app.routes.ts
+++ src/DeliAppClient/src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+﻿import { Routes } from '@angular/router';
 
 //import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
@@ -8,7 +8,7 @@ import { HomeComponent } from './home/home.component';
 //import { ContactComponent } from './contact/contact.component';
 
 export const rootRouterConfig: Routes = [
-  //{ path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   //{ path: 'home', component: HomeComponent },
   //{ path: 'about', component: AboutComponent },
   //{ path: 'github', component: RepoBrowserComponent,
